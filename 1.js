9b9c2a53ce3485a76e78eefcc1072e3554b60380
psbharath619 <psbharath619@gmail.com>
2016-04-10 12:25:22 +0530

    Basic project files

---
+++ 1.js
@@ -0,0 +1,10 @@
+function show_alert()
+{
+			alert("We thank you for your response! Have a nice day! ");
+			location.assign("main.html");
+}
+
+function emg_call()
+{
+	alert("Call this number now to get emergency service in your area!\n +91 8056090208");
+}
